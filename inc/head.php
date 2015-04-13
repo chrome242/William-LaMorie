f71dda56601bb08090d980651e5761f3babf26ba
William LaMorie <wjl10@cornell.edu>
2015-04-13 19:43:08 -0400

    Building framework
    -added head.php with more tags
    -added js for IR10 viewport issue
    -moved some images around to more accurate locations.
    -updated CSS for new font
    -updated file structure

---
+++ inc/head.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <!-- Tags -->
+    <meta name="viewport" content="width=device-width, inital-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="The professional website of William LaMorie, web developer, freelancer, random computer guy">
+    <meta name="author" content="William LaMorie, wjl10@cornell.edu">
+    <meta name="keywords" content="William LaMorie, Ithaca, Web Development, PHP, Python, ">
+    <meta name="designer" content="Naomi Noir, contact@naominoir.com, William LaMorie, wjl10@cornell.edu">
+    <meta name="content" content="William LaMorie, wjl10@cornell.edu">
+    
+    <title>William LaMorie | Developer</title>
+    
+    <!-- Styles and Fonts -->
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>css/normalize.css">
+    <link href='http://fonts.googleapis.com/css?family=Gruppo' rel='stylesheet' type='text/css'>    <link rel="stylesheet" href="css/main.css">
+    <link rel="stylesheet" href="<?php echo BASE_URL; ?>css/responsive.css">
+    
+  </head>
