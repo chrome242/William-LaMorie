ded40c7837ffda60da8bbe1c29d68e0c831152ac
William LaMorie <wjl10@cornell.edu>
2015-04-13 23:22:19 -0400

    moved contact over
    -moved contact from HTML to PHP
    -updated the CSS file to support this new version

---
+++ Content/contact.php
@@ -0,0 +1,27 @@
+
+    <div id="wrapper">
+      <section id="primary">
+        <h3>General Information</h3>
+        <p>Currently, I am unable to take any new clients for the 1st or
+        2nd quarter of 2015, however, I am currently looking to take on new
+        clients in the 3rd and 4th quarter of 2015.</p>
+        <p>I've an experienced full stack developer with expertise in PHP,
+        SQL, JavaScript, JQuerry, HTML, and CSS. I've worked heavly with
+        Twitter Bootstrap, and am confortable working from the ground up
+        on dynamic, responsive sites, however I accell at modernizing flat,
+        unresponsive HTML sites, building simple custom content manage
+        systems from the ground up.</p>
+        <p>I'm hoping to branch out into using some of the more common PHP
+        or Python based Frameworks in the near future, and would be willing to
+        take on projects utilizing these tools at a reduved rate.</p>
+        <p>Please contact me via email or Twitter if possible, and please only contact via phone if the situation is urgent.</p>
+      </section>
+      
+      <section id="secondary">
+        <h3>Contact Details:</h3>
+        <ul class="contact-info">
+          <li class="phone"><a href="tel:607-277-9101">(607)277-9101</a></li>
+          <li class="email"><a href="mailto:william@williamlamorie.com">William@WilliamLaMorie.com</a></li>
+          <li class="twitter"><a href="http://twitter.com/intent/tweet?screen_name=linnaeus242">@Linnaeus242</a></li>
+        </ul>
+      </section>
