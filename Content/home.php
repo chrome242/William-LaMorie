ad83ad531a378c8afba277e5a85669d04b3effc9
William LaMorie <wjl10@cornell.edu>
2015-04-19 22:26:48 -0400

    preparing to tile in hex grid
    -removed unused challenge.css
    -modified main.css to produce multiple divs each with a ul generated
    from the array in home.php
    -modified responsive.css to accommodate changes
    -added more items to home.php for testing

--- Content/home.php
+++ Content/home.php
@@ -1,7 +1,13 @@
 <?php
 $content = array(1 => ["Finger-Lakes-on-Tap.png", "Finger Lakes On Tap", "http://www.fingerlakesontap.com"],
-                 2 => ["Naomi-Noir.png", "Naomi Noir, Designer", "http://www.naominior.com"],
+                 2 => ["Naomi-Noir.png", "Naomi Noir, Designer", "http://www.naominoir.com/"],
                  3 => ["Dating-App.png", "Dating App Demo", "#"], 
                  4 => ["Mike-the-Frog.png", "Mike the Frog Demo", "#"],
                  5 => ["PHP-Quizzer.png", "PHP Quizzer Demo", "#"],
-                 6 => ["Shout-Box.png", "Shout Box Demo", "#"]);
+                 6 => ["Shout-Box.png", "Shout Box Demo", "#"],
+                 7 => ["Finger-Lakes-on-Tap.png", "Finger Lakes On Tap", "http://www.fingerlakesontap.com"],
+                 8 => ["Naomi-Noir.png", "Naomi Noir, Designer", "http://www.naominoir.com/"],
+                 9 => ["Dating-App.png", "Dating App Demo", "#"], 
+                 10 => ["Mike-the-Frog.png", "Mike the Frog Demo", "#"],
+                 11 => ["PHP-Quizzer.png", "PHP Quizzer Demo", "#"],
+                 12 => ["Shout-Box.png", "Shout Box Demo", "#"]);
--- Home/body.php
+++ Home/body.php
@@ -1,24 +1,49 @@
 
     <div id="wrapper">
       <section>
-        <ul id="gallery">
+        <div id="gallery">
+          <ul class="left-row">
 <?php
-  foreach($content as $number => $content)
-  {
-
+  
+  $row_width = 3; // determines how many items to a given row
+  
+  // loops though a 1-indexed array of content
+  foreach($content as $number => $content){
+    
+    // handle div control where needed.
+    if(($number -1) % $row_width == 0  && $number -1 != 0){
+      // every other row should have the right-row class.
+      
+      if (($number -1) % (2*$row_width) != 0){ // if an indented row
+        echo <<< EOT
+          </ul>
+          <ul class="right-row">
+  
+EOT;
+      } else {  // if an un-indented row
+        echo <<< EOT
+          </ul>
+          <ul class="left-row">
+  
+EOT;
+      }
+    }
+  
+  // make each individual image
   $image = PROJECTS.$content[0];
   $desc = $content[1];
   $url = $content[2];
   echo <<< EOT
-          <li>
-            <a target="_blank" href="$url">
-              <img class="button" src="$image" alt="$desc">
-              <p>$desc</p>
-            </a>
-          </li>
+            <li>
+              <a target="_blank" href="$url">
+                <img class="button" src="$image" alt="$desc">
+                <p>$desc</p>
+              </a>
+            </li>
 
 EOT;
   }
   ?>
-        </ul>
+          </ul>
+        </div>
       </section>
